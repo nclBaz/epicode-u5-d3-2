b0d5b727884129017397ebeb42c3a79476e5f3a5
Riccardo Gulin <riccardo@nucleode.com>
2022-11-03 09:49:22 +0100

    Adds basic server configuration

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from "express"
+import listEndpoints from "express-list-endpoints"
+import cors from "cors"
+import {
+  badRequestErrorHandler,
+  forbiddenErrorHandler,
+  genericErrorHandler,
+  notFoundErrorHandler,
+  unauthorizedErrorHandler,
+} from "./errorHandlers.js"
+
+const server = express()
+const port = process.env.PORT || 3001
+
+// ********************************* MIDDLEWARES *****************************
+server.use(cors())
+server.use(express.json())
+
+// ********************************** ENDPOINTS ******************************
+
+// ******************************** ERROR HANDLERS ***************************
+server.use(badRequestErrorHandler)
+server.use(unauthorizedErrorHandler)
+server.use(forbiddenErrorHandler)
+server.use(notFoundErrorHandler)
+server.use(genericErrorHandler)
+
+server.listen(port, () => {
+  console.table(listEndpoints(server))
+  console.log(`Server is listening on port ${port}`)
+})
